be54c160ecc5fdd3fe1646dd6d67b854bfada90d
Chris Troutner <chris.troutner@gmail.com>
2020-03-13 09:49:50 -0700

    fix(price): Updating BCH price and saving state

--- src/lib/util.js
+++ src/lib/util.js
@@ -8,11 +8,14 @@
 
 // const config = require('../../config')
 const fs = require('fs')
+const got = require('got')
 
 const config = require('../../config')
 
 // Winston logger
 const wlogger = require('./wlogger')
+
+const STATE_FILE_NAME = `${__dirname}/../../state/state.json`
 
 class TLUtils {
   // constructor () {}
@@ -27,7 +30,7 @@ class TLUtils {
     try {
       wlogger.silly('entering util.js/saveState().')
 
-      const filename = `${__dirname}/../../state/state.json`
+      const filename = STATE_FILE_NAME
 
       return new Promise((resolve, reject) => {
         fs.writeFile(filename, JSON.stringify(data, null, 2), function (err) {
@@ -49,16 +52,16 @@ class TLUtils {
   }
 
   // Open and read the state JSON file.
-  readState (filename) {
+  readState () {
     try {
       // Delete the cached copy of the data.
-      delete require.cache[require.resolve(filename)]
+      delete require.cache[require.resolve(STATE_FILE_NAME)]
 
-      const data = require(filename)
+      const data = require(STATE_FILE_NAME)
       return data
     } catch (err) {
       wlogger.debug('Error in util.js/readState()')
-      throw new Error(`Could not open ${filename}`)
+      throw new Error(`Could not open ${STATE_FILE_NAME}`)
     }
   }
 
@@ -82,6 +85,43 @@ class TLUtils {
 
   sleep (ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+  // Gets the current spot price of BCH/USD from Coinbase. Returns the previously
+  // saved price from the state.json file if the price can not be retrieved.
+  // It will save the price to the state file when new priceses can be successfully
+  // retrieved.
+  async getPrice () {
+    try {
+      let USDperBCH
+      try {
+        const rawRate = await got(
+          'https://api.coinbase.com/v2/exchange-rates?currency=BCH'
+        )
+        const jsonRate = JSON.parse(rawRate.body)
+        // console.log(`jsonRate: ${JSON.stringify(jsonRate, null, 2)}`);
+
+        USDperBCH = jsonRate.data.rates.USD
+        wlogger.info(`USD/BCH exchange rate: $${USDperBCH}`)
+
+        config.usdPerBCH = USDperBCH
+
+        await this.saveState(config)
+
+        return config.usdPerBCH
+      } catch (err) {
+        wlogger.error(
+          'Coinbase exchange rate could not be retrieved!. Retrieving price from state.'
+        )
+        wlogger.error(err)
+
+        const state = this.readState()
+        return state.usdPerBCH
+      }
+    } catch (err) {
+      wlogger.error('Error in util.js/getPrice()')
+      throw err
+    }
   }
 }
 
