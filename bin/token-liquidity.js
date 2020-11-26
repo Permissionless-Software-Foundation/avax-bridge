ee114e37d504776ae503db0a9025def821e82704
Chris Troutner <chris.troutner@gmail.com>
2020-11-25 18:41:21 -0800

    fix(debugging): Adding debugging code

--- bin/token-liquidity.js
+++ bin/token-liquidity.js
@@ -116,6 +116,7 @@ async function startTokenLiquidity () {
   // Interval to consolidate UTXOs (maintenance)
   setInterval(async function () {
     const hex = await bch.consolidateUtxos()
+    wlogger.info('consolidateUtxos hex: ', hex)
     await bch.broadcastBchTx(hex)
   }, CONSOLIDATE_INTERVAL)
 
